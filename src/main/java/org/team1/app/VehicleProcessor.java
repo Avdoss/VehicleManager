d3bb5f2449624dbfad0ccf587e7c72b3a65ec6e2
Avdeev <avdoss@bk.ru>
2026-09-22 18:14:22 +0300

    Авдеев: В классе VehicleProcessor метод selectVehicles() переименован на sortVehicles, который возвращает отсортированный список транспортных средств. Добавлена реализация этого метода. Добавлен метод getVehicles(). Добавлен многопоточный метод getVehicleNumber() для подсчета количества транспортных средств. Класс VehicleProcessor больше не отправляет event "selection".

--- src/main/java/org/team1/app/VehicleProcessor.java
+++ src/main/java/org/team1/app/VehicleProcessor.java
@@ -1,16 +1,18 @@
 package org.team1.app;
 
 import java.nio.file.OpenOption;
+import java.util.Comparator;
 import java.util.List;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeSupport;
 import java.beans.PropertyChangeListener;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class VehicleProcessor implements PropertyChangeListener
 {
     private final VehicleModel model;
     private Sorter sorter;
-    private List<Vehicle> prevSelectionResult;
+    private List<Vehicle> prevQueryResult;
     private String prevMessage;
 
     private final PropertyChangeSupport pcs;
@@ -33,8 +35,9 @@ public class VehicleProcessor implements PropertyChangeListener
 
     public void saveToFile(String filePath, OpenOption option)
     {
-        if (prevSelectionResult != null)
-            model.saveToFile(prevSelectionResult, filePath, option);
+        if (prevQueryResult != null)
+            prevQueryResult = model.getVehicles();
+        model.saveToFile(prevQueryResult, filePath, option);
     }
 
     public void addVehicle(String type, String[] args)
@@ -42,6 +45,7 @@ public class VehicleProcessor implements PropertyChangeListener
         try {
             Vehicle vehicle = model.getVehicleFactory().createVehicle(type, args);
             model.addVehicle(vehicle);
+            notifyMessage("A new vehicle has been added: " + vehicle.toString());
         } catch (IllegalArgumentException e) {
             notifyMessage("Vehicle creation error: " + e.getMessage());
         }
@@ -52,9 +56,77 @@ public class VehicleProcessor implements PropertyChangeListener
 
     }
 
-    public void selectVehicles(String query)
+    public List<Vehicle> getVehicles()
     {
+        return model.getVehicles();
+    }
 
+    public List<Vehicle> sortVehicles(String field)
+    {
+        Comparator<Vehicle> comparator;
+        switch (field)
+        {
+            case "type":
+                comparator = new TypeComparator();
+                break;
+            case "model":
+                comparator = new ModelComparator();
+                break;
+            case "power":
+                comparator = new PowerComparator();
+                break;
+            case "mileage:":
+                comparator = new MileageComparator();
+                break;
+            default:
+                notifyMessage("Sort error: invalid field name - " + field);
+                return null;
+        }
+        List<Vehicle> vehicles = model.getVehicles();
+        selectSorter(vehicles.size());
+        prevQueryResult = sorter.sort(vehicles, comparator);
+        return List.copyOf(prevQueryResult);
+    }
+
+    public int getVehicleNumber(String type, String[] args)
+    {
+        Vehicle vehicle;
+        try {
+            vehicle = model.getVehicleFactory().createVehicle(type, args);
+        } catch (IllegalArgumentException e) {
+            notifyMessage("Error in counting the number of vehicles: " + e.getMessage());
+            return -1;
+        }
+
+        final int TARGET_ELEMENTS_PER_THREAD = 50;
+        final int MIN_THREAD_NUMBER = 1;
+        final int MAX_THREAD_NUMBER = 4;
+
+        List<Vehicle> vehicles = model.getVehicles();
+        int size = vehicles.size();
+        if (size == 0)
+            return 0;
+        int thread_number = Math.clamp(size / TARGET_ELEMENTS_PER_THREAD, MIN_THREAD_NUMBER, MAX_THREAD_NUMBER);
+        int fragment_size = size / thread_number;
+
+        AtomicInteger counter = new AtomicInteger(0);
+        Thread[] threads = new Thread[thread_number];
+        for(int i = 0; i < thread_number; i++)
+        {
+            int start_index = i * fragment_size;
+            int stop_index = (i == thread_number - 1) ? size - 1 : start_index + fragment_size - 1;
+            SearchTask task = new SearchTask(vehicles, vehicle, start_index, stop_index, counter);
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+        try {
+            for (Thread thread : threads)
+                thread.join();
+        } catch (InterruptedException e) {
+            notifyMessage("Error in counting the number of vehicles: the thread was interrupted");
+            return -1;
+        }
+        return counter.get();
     }
 
     public void addEventListener(PropertyChangeListener listener)
@@ -74,12 +146,6 @@ public class VehicleProcessor implements PropertyChangeListener
         notifyMessage((String)event.getNewValue());
     }
 
-    private void notifySelectionResult(List<Vehicle> vehicles)
-    {
-        pcs.firePropertyChange("selection", prevSelectionResult, List.copyOf(vehicles));
-        prevSelectionResult = vehicles;
-    }
-
     private void notifyMessage(String message)
     {
         this.pcs.firePropertyChange("message", this.prevMessage, message);
@@ -89,5 +155,30 @@ public class VehicleProcessor implements PropertyChangeListener
     private void selectSorter(int count)
     {
 
+    }
+
+    private static final class SearchTask implements Runnable
+    {
+        private final List<Vehicle> vehicles;
+        private final Vehicle desiredVehicle;
+        private final int start_index;
+        private final int stop_index;
+        private final AtomicInteger counter;
+
+        public SearchTask(List<Vehicle> vehicles, Vehicle desiredVehicle, int start_index, int stop_index, AtomicInteger counter)
+        {
+            this.vehicles = vehicles;
+            this.desiredVehicle = desiredVehicle;
+            this.start_index = start_index;
+            this.stop_index = stop_index;
+            this.counter = counter;
+        }
+
+        @Override
+        public void run() {
+            for(int index = start_index; index <= stop_index; index++)
+                if(vehicles.get(index).equals(desiredVehicle))
+                    counter.incrementAndGet();
+        }
     }
 }
