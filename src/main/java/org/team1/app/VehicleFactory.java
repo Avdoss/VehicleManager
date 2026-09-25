339ed55ef6df6767ff3d90eec133b14a8d4f1dca
Avdeev <avdoss@bk.ru>
2026-09-25 12:07:44 +0300

    Авдеев: В класс VehicleFactory добавлены два метода для создания рандомных транспортных средств: createRandomVehicle(), createRandomVehicle(String type).

--- src/main/java/org/team1/app/VehicleFactory.java
+++ src/main/java/org/team1/app/VehicleFactory.java
@@ -3,6 +3,7 @@ package org.team1.app;
 import java.util.List;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Random;
 
 public abstract class VehicleFactory
 {
@@ -28,5 +29,16 @@ public abstract class VehicleFactory
         return availableTypes.contains(type);
     }
 
+    public Vehicle createRandomVehicle()
+    {
+        if (availableTypes.isEmpty())
+            return null;
+        Random random = new Random();
+        int randomIndex = random.nextInt(availableTypes.size());
+        String randomType = availableTypes.stream().toList().get(randomIndex);
+        return createRandomVehicle(randomType);
+    }
+
     public abstract Vehicle createVehicle(String type, String[] args) throws IllegalArgumentException;
+    public abstract Vehicle createRandomVehicle(String type);
 }
